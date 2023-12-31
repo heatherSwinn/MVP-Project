b98e5f51e4b36a2f46e616f804b082c071f3c10f
Heather Swinn <h.swinn@Heathers-MacBook-Air.local>
2023-12-30 17:06:01 -0800

    added flatpickr and updated website

--- app.js
+++ app.js
@@ -20,9 +20,29 @@ app.use(express.json());
 app.get("/exercise", (req, res) => {
     pool.query(`SELECT * FROM workouts`)
         .then((data) => {
-            res.json(data);
-        });
+            res.json(data.rows);
+        })
+        .catch((error) => {
+            console.error("Error fetching exercises: ", error);
+            res.status(500).send("Internal Server Error");
+        })
 });
+
+app.post("/exercise", (req, res) => {
+    const { userName, workout, duration, date } = req.body;
+
+    pool.query(
+        `INSERT INTO workouts (name, duration, date) VALUES ($1, $2, $3) RETURNING *`
+        [workout, duration, date]
+    )
+    .then((data) => {
+        res.json(data.rows[0]);
+    })
+    .catch((error) => {
+        console.error("Error inserting exercise: ", error);
+        res.status(500).send("Internal Server Error");
+    })
+})
 
 app.listen(expressPort, () => {
     console.log(`Listening on port ${expressPort}...`);
--- public/index.js
+++ public/index.js
@@ -1,5 +1,57 @@
-fetch("/exercise")
-  .then((res) => res.json())
-  .then((data) => {
-    console.log("completed exercises: ", data);
-  });
+const form = document.getElementById("workoutForm");
+const exerciseResults = document.getElementById("exerciseResults");
+
+//initialize flatpickr for the date input
+flatpickr("#date", {
+    enableTime: true,
+    altInput: true,
+    altFormat: "F j, Y",
+    dateFormat: "Y-m-d",
+})
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const formData = new FormData(form);
+    const exerciseData = {
+        userName: formData.get("userName"),
+        exerciseName: formData.get("workout"),
+        duration: formData.get("duration"),
+        date: formData.get("date"),
+    };
+
+    fetch("/exercise", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(exerciseData),
+    })
+    .then((res) => res.json())
+    .then((data) => {
+        console.log("Submited exercise:", data);
+        displayExerciseResults();
+    })
+    .catch((error) => {
+        console.error("Error submitting exercise: ", error);
+    })
+})
+
+function displayExerciseResults() {
+    fetch("/exercise")
+        .then((res) => res.json())
+        .then((data) => {
+            exerciseResults.innerHTML = "";
+            data.forEach((exercise) => {
+                const exerciseItem = document.createElement("div");
+                exerciseItem.textContent = `${exercise.name} \n${exercise.duration} minutes \n${exercise.date}`;
+                exerciseResults.appendChild(exerciseItem);
+            });
+        })
+        .catch((error) => {
+            console.error("Error fetching exercises: ", error);
+        })
+}
+
+//initial display of exercises
+displayExerciseResults();
